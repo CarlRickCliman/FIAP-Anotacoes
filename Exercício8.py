3734454fd4ebc523f943b882b9a4d003e66c22f4
cl.contapessoal@gmail.com <cl.contapessoal@gmail.com>
2026-09-09 21:43:03 -0300

    Adicionado exercícios de python

---
+++ Exercício8.py
@@ -0,0 +1,7 @@
+distancia = float(input("Digite a distância percorrida(em km): "))
+
+combustivelgasto = float(input("Digite a quantidade gasta de combustível(em litros): "))
+
+consumomedio = combustivelgasto/distancia
+
+print(f'Você teve um consumo médio de {consumomedio} litros por km')
