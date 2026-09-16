934f8a8546167b840d89c9d5191d41dab20e31cd
carlrickcliman <cl.contapessoal@gmail.com>
2026-09-16 20:37:10 -0300

    Apagado arquivos .py

--- Exercício3.py
+++
@@ -1,5 +0,0 @@
-peso = float(input("Digite seu peso: "))
-altura = float(input("Digite sua altura: "))
-
-IMC = peso/altura
-print(f"Seu Indice de Massa Corporal é: {IMC}")
--- Exercício4.py
+++
@@ -1,9 +0,0 @@
-salario = float(input('Digite seu salário Atual: '))
-
-desconto = float(0.15)
-promocao = salario * desconto
-
-salarioAtual = float(salario+promocao)
-
-print(f'Seu salário atual, com um aumento de 15% é: {salarioAtual}')
-
